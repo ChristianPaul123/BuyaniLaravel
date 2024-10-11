98b0c2f363ebfa28d7071ee8406a9c09f3f3ac88
ChristianPaul123 <christianpaulespares1@gmail.com>
2024-10-11 21:07:14 +0800

    database formulation

--- app/Http/Controllers/UserProductController.php
+++ app/Http/Controllers/UserProductController.php
@@ -1,10 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Validation\Rule;
 
 use Illuminate\Http\Request;
 
 class UserProductController extends Controller
 {
-    //
+ public function showConsumerProduct()
+ {
+    $products = Product::where('product_status', 1)->get();
+    return view('user.product.show', compact('products'));
+ }
+ public function showConsumerProductDetails($id)
+ {
+    $product = Product::find($id);
+    return view('consumer.product-details', compact('product'));
+ }
+ public function showConsumerProductCategory($id)
+ {
+    $category = Category::find($id);
+    $products = Product::where('category_id', $id)->where('status', 1)->get();
+    return view('consumer.product-category', compact('products', 'category'));
+ }
+
+
 }
