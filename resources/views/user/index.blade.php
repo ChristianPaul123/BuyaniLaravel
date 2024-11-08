70e50b876fd7b3f54742a4a947f861d8f5ca75f0
Kian <kianchestertolos.chapman@bicol-u.edu.ph>
2024-11-08 22:23:54 +0800

    main title page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,43 +4,115 @@
 
 @push('styles')
 <style>
-    .custom-font-content {
+    .body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .body {
         font-family: 'Poppins', sans-serif;
-        font-weight: bold;
-        color: aliceblue;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background-image: url('{{ asset('img/title/farm.jpg') }}'); /* Replace with your image URL */
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        color: white;
+    }
+
+    .center-content {
+        padding: 30px;
+        border-radius: 15px;
+        text-align: center;
+    }
+
+    .buyani {
+        font-weight: 700;
+        font-size: 7rem;
+        margin-bottom: 40px;
+        padding: 50px;
+        color: transparent;
+        -webkit-text-stroke: 1px white;
+        text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+    }
+
+    .buyani .buy {
+        color: green;
+    }
+
+    .buyani .ani {
+        color: orange;
+    }
+
+    .clickable-div {
+        width: 270px;
+        background-color: green;
+        color: white;
+        padding: 15px;
+        border-radius: 8px;
+        text-align: center;
+        cursor: pointer;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 1.2rem;
+        transition: transform 0.2s ease;
+    }
+
+    img {
+        width: 20%;
+        margin-bottom: 15px;
+    }
+
+    .clickable-div:hover {
+        transform: translateY(-3px);
+    }
+
+    .clickable-div:active {
+        transform: translateY(1px);
     }
 </style>
 @endpush
 @section('x-content')
-    @include('user.includes.navbar-consumer')
     @if (session('message'))
     <div class="alert alert-success">
         {{ session('message') }}
     </div>
     @endif
     <!--CONTENT-->
+    <div class="body">
+        <div class="container center-content">
+            <!-- BuyAni Heading -->
+            <h1 class="buyani">
+                <span class="buy">Buy</span><span class="ani">Ani</span>
+            </h1>
 
-    <div class=" d-flex row custom-font-content g-2">
-        <div class="col-lg-6 border border-primary login-card d-flex flex-row align-items-center justify-content-center" style="background-color:green; height: 80vh;">
-          <form action="{{ route('user.register') }}" method="GET">
-              <input type="hidden" name="user_type" value="1">
-              <button type="submit" class="btn btn-primary">Register as Consumer</button>
-          </form>
-          <form action="{{ route('user.register') }}" method="GET">
-              <input type="hidden" name="user_type" value="2">
-              <button type="submit" class="btn btn-primary">Register as Farmer</button>
-          </form>
+            <!-- Clickable Divs with Icons in Separate Columns -->
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="{{ route('user.login') }}" method="GET">
+                        <input type="hidden" name="user_type" value="1">
+                        <button type="submit" class="clickable-div">
+                            <img src="{{ asset('img/title/consumer.png') }}" alt="Consumer Icon">
+                            <h5>Interact as Consumer</h5>
+                        </button>
+
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <form action="{{ route('user.login') }}" method="GET">
+                        <input type="hidden" name="user_type" value="2">
+                        <button type="submit" class="clickable-div">
+                            <img src="{{ asset('img/title/farmer.png') }}" alt="Farmer Icon">
+                            <h5>Interact as Farmer</h5>
+                        </button>
+
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-6 border border-primary login-card d-flex flex-row align-items-center justify-content-center" style="background-color:rgb(255, 128, 0); height: 80vh;">
-            <form action="{{ route('user.login') }}" method="GET">
-                <input type="hidden" name="user_type" value="1">
-                <button type="submit" class="btn btn-primary">login as Consumer</button>
-            </form>
-            <form action="{{ route('user.login') }}" method="GET">
-                <input type="hidden" name="user_type" value="2">
-                <button type="submit" class="btn btn-primary">login as Farmer</button>
-            </form>
-          </div>
     </div>
 @endsection
 @section('scripts')
