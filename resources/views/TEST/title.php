c8b00ff4d827bda8560431348d579289f9b24976
Carmillo kim <carmillokim.reola@gmail.com>
2024-11-08 23:29:18 +0800

    about-us design

---
+++ resources/views/TEST/title.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap Layout Example</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <style>
+        *{
+            border: 1px solid black;
+        }
+    </style>
+
+</head>
+<body>
+
+  <div class="container text-center">
+    <!-- Row 1 with centered h1 -->
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <h1>BuyAni</h1>
+        </div>
+    </div>
+    
+    <!-- Row 2 with 2 columns -->
+    <div class="row">
+        <div class="col">
+            <h2>Interact as Consumer</h2>
+        </div>
+        <div class="col">
+            <h2>Interact as Consumer</h2>
+        </div>
+    </div>
+  </div>
+
+</body>
+</html>
--- resources/views/about-us-consumer.blade.php
+++ resources/views/about-us-consumer.blade.php
@@ -11,6 +11,7 @@
     .team-member {
         margin-bottom: 30px;
     }
+
 </style>
 @endpush
 
@@ -18,34 +19,42 @@
  @include('user.includes.navbar-consumer')
 
 <!-- About Us Section -->
-<div class="container about-section" style="background-image: url('{{ asset('img/stockImg4.png') }}'); background-repeat: no-repeat; background-size: cover; background-position: center;">
-    <h2 class="text-center mb-4">About Us</h2>
-    <p class="text-center">We are a team of passionate individuals committed to delivering the best services to our clients.</p>
+<div class="container about-section" style="background-image: url('{{ asset('img/background.jpg') }}'); background-repeat: no-repeat; background-size: cover; background-position: center;">
+    <h2 class="text-center mb-4 text-white">About Us</h2>
+    <p class="text-center text-white">We are a team of passionate individuals committed to delivering the best services to our clients.</p>
     <div class="row">
+        <!-- Team Member 1 -->
         <div class="col-lg-3 col-md-6 text-center team-member">
-            <img src="https://via.placeholder.com/150" class="rounded-circle mb-2" alt="Team Member">
-            <h4>John Doe</h4>
-            <p>Founder & CEO</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <img src="{{ asset('img/about_profile/reola_prof.png') }}" class="rounded-circle mb-2 img-fluid" style="width: 150px; height: 150px; object-fit: cover;" alt="Team Member">
+            <h4 class="text-white">Carmilo Kim Reola</h4>
+            <p class="text-white">Founder & CEO</p>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
+
+        <!-- Team Member 2 -->
         <div class="col-lg-3 col-md-6 text-center team-member">
-            <img src="https://via.placeholder.com/150" class="rounded-circle mb-2" alt="Team Member">
-            <h4>Jane Smith</h4>
-            <p>Chief Operating Officer</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <img src="{{ asset('img/about_profile/chapman_prof.png') }}" class="rounded-circle mb-2 img-fluid" style="width: 150px; height: 150px; object-fit: cover;" alt="Team Member">
+            <h4 class="text-white">Kian Chester Chapman</h4>
+            <p class="text-white">Chief Operating Officer</p>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
+
+        <!-- Team Member 3 -->
         <div class="col-lg-3 col-md-6 text-center team-member">
-            <img src="https://via.placeholder.com/150" class="rounded-circle mb-2" alt="Team Member">
-            <h4>Emily Johnson</h4>
-            <p>Chief Technology Officer</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <img src="{{ asset('img/about_profile/espares_prof.png') }}" class="rounded-circle mb-2 img-fluid" style="width: 150px; height: 150px; object-fit: cover;" alt="Team Member">
+            <h4 class="text-white">Christian Paul Espares</h4>
+            <p class="text-white">Chief Technology Officer</p>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
+
+        <!-- Team Member 4 -->
         <div class="col-lg-3 col-md-6 text-center team-member">
-            <img src="https://via.placeholder.com/150" class="rounded-circle mb-2" alt="Team Member">
-            <h4>Michael Brown</h4>
-            <p>Marketing Manager</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <img src="{{ asset('img/about_profile/vista_prof.png') }}" class="rounded-circle mb-2 img-fluid" style="width: 150px; height: 150px; object-fit: cover;" alt="Team Member">
+            <h4 class="text-white">Melinda Vista</h4>
+            <p class="text-white">Marketing Manager</p>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
     </div>
 </div>
+
 @endsection
