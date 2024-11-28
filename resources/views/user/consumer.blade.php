7d09e117ea78cb53947eabfc218047ea0dd7bbcc
Kian <kianchestertolos.chapman@bicol-u.edu.ph>
2024-11-29 00:25:22 +0800

    Home Page: Updated

--- resources/views/user/consumer.blade.php
+++ resources/views/user/consumer.blade.php
@@ -3,96 +3,160 @@
 @section('title', 'consumer Page') <!-- Defining a title for this view -->
 
 @push('styles')
+<style>
+    /* Section 1 */
+    *{
+        /* border: 1px solid black; */
+    }
+
+    .section-1{
+        height: 705px;
+        padding: 100px 100px 50px 100px;
+        background-image: url({{ asset('img/stockImg3.png') }});
+    }
+    .section-1 .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .section-1 h1{
+        font-size: 60px;
+        font-weight: bold;
+        padding: 0;
+    }
+    .section-1 h1:nth-of-type(1){
+        color: #ffffff;
+    }
+    .section-1 h1:nth-of-type(2) span:nth-of-type(1){
+        color: #ffa500;
+    }
+    .section-1 h1:nth-of-type(2) span:nth-of-type(2){
+        color: #00cc1a;
+    }
+    .section-1 h4{
+        color: #ffffff;
+    }
+    .section-1 button{
+        width: 150px;
+        height: 40px;
+        border: 3px solid #ffa500;
+        border-radius: 7px;
+        background-color: #fff8dd;
+        color: #ffa500;
+        transition: all 0.5s ease;
+    }
+    .section-1 button:hover{
+        scale: 1.1;
+    }
+
+    /* Section 2 */
+    .section-2{
+        padding: 50px 100px 50px 100px;
+
+    }
+    .section-2 .row:nth-of-type(1){
+        text-align: center;
+    }
+    .promo-img{
+        border-radius: 15px;
+        height: 400px;
+        width: 600px;
+    }
+
+    /* Section 3 */
+    .section-3{
+        padding: 50px 100px 50px 100px;
+
+    }
+    .section-3 .left-panel{
+        text-align: center;
+    }
+    .section-3 .left-panel h3{
+        margin-bottom: 20px;
+    }
+    .section-3 .right-panel .container{
+        border: 5px solid #00cc1a;
+        border-radius: 15px;
+    }
+    .section-3 button{
+        width: 150px;
+        height: 40px;
+        border: 3px solid #ffa500;
+        border-radius: 7px;
+        background-color: #ffa500;
+        color: #fff8dd;
+        transition: all 0.5s ease;
+    }
+    .section-3 button:hover{
+        scale: 1.1;
+    }
+
+</style>
 
 @endpush
 @section('x-content')
      @include('user.includes.navbar-consumer')
-     {{-- <form action="{{ route('user.logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="btn btn-danger">Logout</button>
-    </form>
-     <h1>Welcome to Consumer Dashboard, {{ auth()->guard('user')->user()->username }}</h1> --}}
+     <section class="main-page">
 
-    <!-- Main Page -->
-    <section class="hero-section d-flex align-items-center" style="background-image: url({{ asset('img/stockImg3.png') }}); background-repeat: no-repeat; background-size: cover; background-position: center; height: 650px;">
-        <div class="container">
-            <h1 class="display-4" style="color: #FFFF; font-size: 60px; font-weight: bold; margin-bottom: -10px;">EMPOWER FARMERS</h1>
-            <h1 style="color: #F39634; font-size: 60px; font-weight: bold; display: inline;">ENRICH </h1><h1 style="color: #69A543; font-size: 60px; font-weight: bold; display: inline;">COMMUNITIES</h1>
-            <p class="lead" style="color: #FFFF; font-size: 22px; font-weight: bold;">BuyAni, Where Every Purchase is a Celebration of Hard Work and Fresh Harvests</p>
-            <a href="/user/consumer/products" class="btn btn-lg" style="background-color: #F39634; color: #FFFF;">SHOP NOW!</a>
+        <!-- Section 1 -->
+        <div class="row section-1">
+            <div class="container">
+                <h1 style="margin-bottom: -10px;">EMPOWER FARMERS</h1>
+                <h1>
+                    <span>ENRICH</span>
+                    <span>COMMUNITIES</span>
+                </h1>
+                <h4>BuyAni, Where Every Purchase is a Celebration of Hard Work and Fresh Harvests</h4>
+                <button>Shop Now!</button>
+            </div>
         </div>
-    </section>
 
-    <!-- Our Products -->
-    <section class="text-center p-3">
-        <h1>Our Products</h1>
-        <p>Freshly delivered from our local farmers!</p>
-        <div class="container-fluid">
-            <div class="d-flex flex-row md-4">
-                @foreach ($products as $product)
-                <div class="col-md-3 offset-md-2 m-1">
-                    <div class="card">
-                        <div class="card-header d-flex align-items-center justify-content-center">
-                          <div class="ms-3">
-                            <h6 class="mb-0 fs-sm text-center">{{ $product->product_name }}</h6>
-                          </div>
-                        </div>
-                        <img src="{{ asset( "$product->product_pic" ) }}" class="card-img-top" alt="products {{ asset( "$product->product_name" ) }}" />
-                        <div class="card-body">
-                            <p class="card-text">{{ $product->product_details }}</p>
-                        </div>
-                        <div class="card-footer d-flex">
-                            <button class="btn btn-subtle" type="button"><i class="fas fa-heart fa-lg"></i></button>
-                          <a class="btn btn-primary w-100 p-2 me-auto fw-bold" href=" {{ route('user.consumer.product.view', $product->id)}}">View</a>
-                          <button class="btn btn-subtle" type="button"><i class="fas fa-share fa-lg"></i></button>
-                        </div>
-                      </div>
+        <!-- Section 2 -->
+        <div class="row section-2">
+            <div class="row">
+                <h2>Our Products</h2>
+                <h4>Freshly delivered from our local farmers!</h4>
+            </div>
+            <div class="row">
+                palagay dito basic na card layout ng products
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <img class="promo-img" src= "{{ asset('img/stockImg.png') }}">
                 </div>
-                @endforeach
+                <div class="col-md-6 promo-text pt-5">
+                    <h3>Lorem Ipsum</h3>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 promo-text pt-5">
+                    <h3>Lorem Ipsum</h3>
+                    <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h6>
+                </div>
+                <div class="col-md-6 text-center">
+                    <img class="promo-img" src= "{{ asset('img/stockImg2.png') }}">
+                </div>
             </div>
         </div>
-    </section>
 
-    <!-- Details -->
-     <section>
-        <div class="row p-5">
-            <div class="col-md-6 d-flex justify-content-center" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100">
-                <img src= "{{ asset('img/stockImg.png') }}" alt="Landscape Image" width="600" height="400" style="border-radius: 20px;">
-            </div>
-            <div class="col-md-6 px-5">
-                <h4>Lorem Ipsum</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            </div>
-        </div>
-        <div class="row p-5">
-            <div class="col-md-6 px-5">
-                <h4>Lorem Ipsum</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            </div>
-            <div class="col-md-6 d-flex justify-content-center" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
-                <img src="../img/stockImg2.png" alt="Landscape Image" width="600" height="400" style="border-radius: 20px;">
-            </div>
-        </div>
-     </section>
-
-     <!-- Contact Us -->
-     <section class="p-3" style="background-image: url({{ asset('img/stockImg5.png') }}); background-repeat: no-repeat; background-size: cover; background-position: center; height: 450px;">
-        <div class="row h-100 d-flex align-items-center">
-            <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
-                <h3 class="pb-2" style="color:chartreuse;">Contact Us For Inquiries!</h3>
+        <!-- Section 3 -->
+        <div class="row section-3">
+            <div class="col-md-6 left-panel">
+                <h3>Contact Us For Inquiries!</h3>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d294.38162674181825!2d123.71843815368132!3d13.15098642433476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a1015eccf097cb%3A0x8e91fc8310d08926!2sVKY%20FRUITS%20AND%20VEGETABLES%20TRADING!5e0!3m2!1sen!2sph!4v1726749600004!5m2!1sen!2sph"
                     width="400"
                     height="300"
-                    style="border: 2px solid #000; border-radius: 20px;"
+                    style="border: 4px solid #000; border-radius: 20px;"
                     allowfullscreen=""
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
             </div>
-            <div class="col-md-6 d-flex align-items-center">
-                <div class="container p-3" style="width:500px; background-color: #5DC14C; border: solid black 2px; border-radius: 20px">
-                    <h2 class="text-center">Contact Form</h2>
+            <div class="col-md-6 right-panel">
+                <div class="container p-2">
+                    <h3>Contact Form</h3>
                     <form>
                         <div class="form-group p-1">
                             <label for="name">Name</label>
@@ -104,15 +168,15 @@
                         </div>
                         <div class="form-group p-1">
                             <label for="message">Message</label>
-                            <textarea class="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+                            <textarea class="form-control scroll" id="message" rows="3" placeholder="Enter your message"></textarea>
                         </div>
                         <div class="text-center pt-1">
-                            <button type="submit" class="btn" style="background-color: #F39634; color: #FFFF;">Send</button>
+                            <button type="submit">Send</button>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+
     </section>
 @endsection
 @section('scripts')
