6714bf0789f245e00389e98740407a5f20bde1e0
ChristianPaul123 <christianpaulespares1@gmail.com>
2025-01-04 14:47:56 +0800

    reviews fix

--- app/Http/Controllers/ReviewManagementController.php
+++ app/Http/Controllers/ReviewManagementController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrderRating;
 use Illuminate\Http\Request;
+use App\Models\ProductRating;
 
 class ReviewManagementController extends Controller
 {
     public function showReviews() {
-        return view('admin.community.review-index');
+        $productRating = ProductRating::with('product','user')->get();
+        $orderRating = OrderRating::with('order','user')->get();
+        return view('admin.community.review-index',compact('productRating','orderRating'));
     }
 }
