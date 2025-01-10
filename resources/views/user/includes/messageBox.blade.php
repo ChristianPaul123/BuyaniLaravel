d36191a5649eb07867682acd9d3a60de37f071de
Kian <kianchestertolos.chapman@bicol-u.edu.ph>
2025-01-11 03:06:34 +0800

    message box fix

--- resources/views/user/includes/messageBox.blade.php
+++ resources/views/user/includes/messageBox.blade.php
@@ -1,31 +1,40 @@
 @if(session('message')) <!-- Check for flash message -->
-<div class="notif d-flex alert alert-dismissible fade show" role="alert" id="flashMessage">
-    <i class="fa-solid fa-info"></i>
-    <div class="container-right">
-        <h5 class="m-0">{{ session('message') }}</h5> <!-- Display the flash message -->
+<div class="notif d-flex alert alert-dismissible fade show notif-message" role="alert" id="flashMessage">
+    <div class="container1 container-message">
+        <i class="fa-solid fa-info icon-modal"></i>
     </div>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="container2">
+        <h5 class="m-0 p-2" style="color: black; font-size: 1rem;">{{ session('message') }}</h5> <!-- Display the flash message -->
+    </div>
+    <i class="bi bi-x exit-modal" role="button" data-bs-dismiss="alert" aria-label="Close"></i>
 </div>
 @endif
 
 @if ($errors->any())
-<div class="notif error d-flex alert alert-dismissible fade show" role="alert" id="flashMessage">
-    <i class="fa-solid fa-face-sad-tear"></i>
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="notif error d-flex alert alert-dismissible fade show notif-error" role="alert" id="flashMessage">
+    <div class="container1 container-error">
+        <i class="bi bi-emoji-frown icon-modal"></i>
+    </div>
+    <div class="container2">
+        {{-- <ul> --}}
+            @foreach ($errors->all() as $error)
+                {{-- <li>{{ $error }}</li> --}}
+                <h5 class="m-0 p-2" style="color: black; font-size: 1rem;">{{ $error }}</h5> <!-- Display the flash message -->
+            @endforeach
+        {{-- </ul> --}}
+    </div>
+    <i class="bi bi-x exit-modal" role="button" data-bs-dismiss="alert" aria-label="Close"></i>
 </div>
 @endif
 
 @if(session('success')) <!-- Check for flash message -->
-<div class="notif success d-flex alert alert-dismissible fade show" role="alert" id="flashMessage">
-    <i class="fa-solid fa-info"></i>
-    <div class="container-right">
-        <h5 class="m-0">{{ session('success') }}</h5> <!-- Display the flash message -->
+<div class="notif success d-flex alert alert-dismissible fade show notif-success" role="alert" id="flashMessage">
+    <div class="container1 container-error">
+        <i class="fa-solid fa-info icon-modal"></i>
     </div>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="container2">
+        <h2 class="m-0 pl-2" style="color: black; font-size: 1rem;">{{ session('success') }}</h2> <!-- Display the flash message -->
+    </div>
+    <i class="bi bi-x exit-modal" role="button" data-bs-dismiss="alert" aria-label="Close"></i>
 </div>
 @endif
