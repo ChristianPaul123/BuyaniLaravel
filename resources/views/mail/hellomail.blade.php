b47cebdafd09fb77317b28a6f93ac17427c51784
ChristianPaul123 <christianpaulespares1@gmail.com>
2025-01-13 07:00:50 +0800

    fixed host site for images

--- resources/views/mail/hellomail.blade.php
+++ resources/views/mail/hellomail.blade.php
@@ -83,7 +83,7 @@
 <body>
     <div class="container">
         <div class="header">
-            <img src="{{ asset('img/buyanicommece_logo.png') }}" alt="Buyani Logo">
+            <img src="{{ url('https://buyanicommerce.bsitcps.com/public/img/buyanicommece_logo.png') }}" alt="Buyani Logo">
             <h1>Buyanicommerce</h1>
             <h2>Thank you for Registering to Buyani Shop</h2>
         </div>
