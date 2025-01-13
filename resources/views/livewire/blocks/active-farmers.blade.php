0df00940faf9264e5d8edad3df925cc7cbda9fe6
Kian <kianchestertolos.chapman@bicol-u.edu.ph>
2025-01-13 18:54:53 +0800

    redesign dashboard

--- resources/views/livewire/blocks/active-farmers.blade.php
+++ resources/views/livewire/blocks/active-farmers.blade.php
@@ -1,6 +1,9 @@
-<div class="card">
-    <div class="card-body">
-        <h4>Active Farmers</h4>
-        <p>{{ $activeFarmers }}</p>
+<div class="card" style="color: #B71C1C;">
+    <div class="card-body text-title-container">
+        <h4 class="text-title">Active Farmers</h4>
+        <div class="cont" style="margin-bottom: -20px; color: #F44336">
+            <i class="bi bi-person-fill-check"></i>
+            <p class="counter">{{ $activeFarmers }}</p>
+        </div>
     </div>
 </div>
--- resources/views/livewire/blocks/total-users.blade.php
+++ resources/views/livewire/blocks/total-users.blade.php
@@ -1,6 +1,9 @@
-<div class="card">
-    <div class="card-body">
-        <h4>Total Registered Users</h4>
-        <p>{{ $totalUsers }}</p>
+<div class="card" style="color: #2C5DA8;">
+    <div class="card-body text-title-container">
+        <h4 class="text-title">Total Registered Users</h4>
+        <div class="cont" style="margin-bottom: -20px; color: #4A90E2;">
+            <i class="bi bi-people-fill"></i>
+            <p class="counter">{{ $totalUsers }}</p>
+        </div>
     </div>
 </div>
