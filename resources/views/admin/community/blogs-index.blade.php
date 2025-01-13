681999d1b934a6ff242c461d03e87dd048de1d3b
ChristianPaul123 <christianpaulespares1@gmail.com>
2025-01-14 03:44:55 +0800

    blog admin fix

--- resources/views/admin/community/blogs-index.blade.php
+++ resources/views/admin/community/blogs-index.blade.php
@@ -19,30 +19,10 @@
 
         <section class="col-md-10 ml-sm-auto col-lg-10 px-3 py-5 overflow-y-scroll main-section">
                        {{-- if there is any errors --}}
-        @session('message')
-        <div class=" mx-3 my-2 px-3 py-2 alert alert-success">
-            <button type="button" class="close  btn btn-success" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            {{ session('message') }}
-        </div>
-       @endsession
+
+            @include('admin.includes.messageBox')
 
        {{-- if there's errors --}}
-        @if ($errors->any())
-
-        <div class="alert alert-danger mx-3 my-2 px-3 py-2">
-            <button type="button" class="close btn btn-danger" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-
-        </div>
-        @endif
 
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-1 pb-2 mb-3 border-bottom">
             <h1 class="h2">Blogs</h1>
