0f5c4e28b3e8bcccbd02ad88e8107c4eb2f9f7b0
ChristianPaul123 <christianpaulespares1@gmail.com>
2025-01-16 11:10:24 +0800

    changed: total orders to pending orders

--- resources/views/livewire/blocks/total-orders.blade.php
+++ resources/views/livewire/blocks/total-orders.blade.php
@@ -1,6 +1,6 @@
 <div class="card" style="color: #FF8F00;">
     <div class="card-body text-title-container">
-        <h4 class="text-title">Total Orders</h4>
+        <h4 class="text-title">New Orders</h4>
         <div class="cont" style="margin-bottom: -20px; color:#FFC107;">
             <i class="bi bi-box-seam-fill"></i>
             <p class="counter">{{ $totalOrders }}</p>
